7dc7f898a975e62119905c76f4658daa3e02fc16
full-fool <cyq_2210@163.com>
2014-06-13 16:55:31 +0800

    finish PokerPlayer

--- Classes/PokerPlayer.cpp
+++ Classes/PokerPlayer.cpp
@@ -1,1 +1,18 @@
 #include "Poker.h"
+
+void PokerPlayer::selectCards(PokerCard *card)
+{
+	selectedCards.insert(selectedCards.end()-1, card);
+}
+
+void PokerPlayer::moveCardToDeckAtIndex(PokerCard *card, PokerDeck *deck, int index)
+{
+	card->deck->removeCard(card);
+	deck->insertCardatIndex(card, index);
+}
+
+std::string PokerPlayer::toJSONString()
+{
+	std::string a = "yes";
+	return a;
+}
--- Classes/PokerPlayer.h
+++ Classes/PokerPlayer.h
@@ -1,7 +1,31 @@
 #ifndef __POKER_PLAYER_H__
 #define __POKER_PLAYER_H__
 
+#include <string>
+#include <vector>
+class PokerDeck;
 class PokerPlayer{
+	//ID of player
+	std::string ID;
+
+	//Name of player
+	std::string name;
+
+	//cards selected by the player
+	std::vector<PokerCard*> selectedCards;
+
+	//player's decks
+	std::vector<PokerDeck*> decks;
+
+	//player's base cards
+	PokerDeck *pocket;
+
+	void selectCards(PokerCard *card);
+
+	void moveCardToDeckAtIndex(PokerCard *card, PokerDeck *deck, int index);
+
+	std::string toJSONString();
+
 };
 
 #endif
